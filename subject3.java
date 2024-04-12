91d5764548b92087835dc3009bc7ea1f989b443c
koyuki0214 <165571467+koyuki0214@users.noreply.github.com>
2024-04-12 15:09:34 +0900

    Add files via upload

---
+++ subject3.java
@@ -0,0 +1,13 @@
+public class subject3 {
+    public static void main(String[] args){
+        int i = 35;
+        int j = 24;
+        System.out.println(++i);
+        System.out.println(i++);
+        System.out.println(i - j);
+        int k = 3 >>> 1;
+        System.out.println(k);
+        System.out.println(i>j);
+        System.out.println(k>i);
+    }
+}
